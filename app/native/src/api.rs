50ce98b1442b38e73d20bfeb59da23073679ff05
KyleUltimate <shiue.kyle@gmail.com>
2023-07-09 11:03:17 +0800

    fix: u

--- app/native/src/api.rs
+++ app/native/src/api.rs
@@ -6,14 +6,13 @@ use tokio::sync::{Mutex, RwLock};
 pub use crate::api::minecraft::get_progress;
 
 pub struct Progress {
-    speed: f64,
-    percentages: f64,
-    current_size: f64,
-    total_size: f64,
+    pub speed: f64,
+    pub percentages: f64,
+    pub current_size: f64,
+    pub total_size: f64,
 }
 
 #[tokio::main(flavor = "current_thread")]
 pub async fn test(stream: StreamSink<Progress>) -> anyhow::Result<()> {
-    get_progress(stream).await;
-    Ok(())
+    get_progress(stream).await
 }
