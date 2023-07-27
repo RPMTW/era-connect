6cd4d80e618fe8dd92762760ae60d56816a2f48f
菘菘 <rrt467778@gmail.com>
2023-07-27 23:32:33 +0800

    Update widget_test.dart

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -1,6 +1,9 @@
 // import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   // Your tests go here.
+  test('Empty Test', () {
+    print('Hello, World!');
+  });
 }
