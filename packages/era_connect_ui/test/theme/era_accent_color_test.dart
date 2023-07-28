c819a4a44bc266fe48c22d11556f9fb3b5838759
菘菘 <rrt467778@gmail.com>
2023-07-28 12:59:07 +0800

    ci: better generate coverage script

--- packages/era_connect_ui/test/theme/era_accent_color_test.dart
+++ packages/era_connect_ui/test/theme/era_accent_color_test.dart
@@ -6,19 +6,19 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   group('Check accent colors', () {
     test('green color', () {
-      expect(EraAccentColor.green.color, equals(const Color(0x0014ae5c)));
+      expect(EraAccentColor.green.color, equals(const Color(0xFF14ae5c)));
     });
 
     test('blue color', () {
-      expect(EraAccentColor.blue.color, equals(const Color(0x007caed3)));
+      expect(EraAccentColor.blue.color, equals(const Color(0xFF7caed3)));
     });
 
     test('yellow color', () {
-      expect(EraAccentColor.yellow.color, equals(const Color(0x00d3e950)));
+      expect(EraAccentColor.yellow.color, equals(const Color(0xFFd3e950)));
     });
 
     test('purple color', () {
-      expect(EraAccentColor.purple.color, equals(const Color(0x00d3e950)));
+      expect(EraAccentColor.purple.color, equals(const Color(0xFF9747FF)));
     });
   });
 }
