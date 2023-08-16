e08466f798f6db3f2aaf97c0bee82b9244e172aa
菘菘 <rrt467778@gmail.com>
2023-08-16 15:43:25 +0800

    fix: dart binding

--- app/lib/api/gen/bridge_generated.dart
+++ app/lib/api/gen/bridge_generated.dart
@@ -457,7 +457,7 @@ class NativeWire implements FlutterRustBridgeWireBase {
       : _lookup = lookup;
 
   void store_dart_post_cobject(
-    int ptr,
+    DartPostCObjectFnType ptr,
   ) {
     return _store_dart_post_cobject(
       ptr,
@@ -465,10 +465,10 @@ class NativeWire implements FlutterRustBridgeWireBase {
   }
 
   late final _store_dart_post_cobjectPtr =
-      _lookup<ffi.NativeFunction<ffi.Void Function(ffi.Int)>>(
+      _lookup<ffi.NativeFunction<ffi.Void Function(DartPostCObjectFnType)>>(
           'store_dart_post_cobject');
-  late final _store_dart_post_cobject =
-      _store_dart_post_cobjectPtr.asFunction<void Function(int)>();
+  late final _store_dart_post_cobject = _store_dart_post_cobjectPtr
+      .asFunction<void Function(DartPostCObjectFnType)>();
 
   Object get_dart_object(
     int ptr,
@@ -697,10 +697,9 @@ class NativeWire implements FlutterRustBridgeWireBase {
 final class _Dart_Handle extends ffi.Opaque {}
 
 final class wire_UILayoutValue_CompletedSetup extends ffi.Struct {
+  @ffi.Bool()
   external bool field0;
 }
-
-typedef bool = ffi.NativeFunction<ffi.Int Function(ffi.Pointer<ffi.Int>)>;
 
 final class UILayoutValueKind extends ffi.Union {
   external ffi.Pointer<wire_UILayoutValue_CompletedSetup> CompletedSetup;
@@ -712,3 +711,8 @@ final class wire_UILayoutValue extends ffi.Struct {
 
   external ffi.Pointer<UILayoutValueKind> kind;
 }
+
+typedef DartPostCObjectFnType = ffi.Pointer<
+    ffi.NativeFunction<
+        ffi.Bool Function(DartPort port_id, ffi.Pointer<ffi.Void> message)>>;
+typedef DartPort = ffi.Int64;
